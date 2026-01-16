bde9264c1818cdf9b92c26f44f793e42b19bca7d
Ju102 <jlordonez02@gmail.com>
2026-01-16 11:33:21 +0100

    Add project files.

---
+++ NetCoreAdoNet/Form08CrudDepartamentos.cs
@@ -0,0 +1,86 @@
+﻿using NetCoreAdoNet.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace NetCoreAdoNet.Repositories
+{
+    public partial class Form08CrudDepartamentos : Form
+    {
+        RepositoryDepartamentos repo;
+
+        public Form08CrudDepartamentos()
+        {
+            InitializeComponent();
+            this.repo = new RepositoryDepartamentos();
+            this.LoadDepartamentos();
+        }
+
+        private async void LoadDepartamentos()
+        {
+            List<Departamento> departamentos = await this.repo.GetDepartamentosAsync();
+
+            this.lstDepartamentos.Items.Clear();
+
+            foreach (Departamento departamento in departamentos) 
+            {
+                this.lstDepartamentos.Items.Add(departamento.IdDepartamento + " - " + departamento.Nombre + " - " + departamento.Localidad);
+            }
+        }
+
+        private async void btnInsertar_Click(object sender, EventArgs e)
+        {
+            Departamento departamento = new Departamento();
+
+            departamento.IdDepartamento = int.Parse(this.txtId.Text);
+            departamento.Nombre = this.txtNombre.Text;
+            departamento.Localidad = this.txtLocalidad.Text;
+
+            int registros = await this.repo.AddDepartamentoAsync(departamento);
+
+            MessageBox.Show("Registros insertados: " + registros);
+
+            txtId.Clear();
+            txtNombre.Clear();
+            txtLocalidad.Clear();
+
+            this.LoadDepartamentos();
+        }
+
+        private async void btnModificar_Click(object sender, EventArgs e)
+        {
+            Departamento departamento = new Departamento();
+
+            departamento.IdDepartamento = int.Parse(this.txtId.Text);
+            departamento.Nombre = this.txtNombre.Text;
+            departamento.Localidad = this.txtLocalidad.Text;
+
+            int registros = await this.repo.UpdateDepartamentoAsync(departamento);
+
+            MessageBox.Show("Registros modificados: " + registros);
+            
+            txtId.Clear();
+            txtNombre.Clear();
+            txtLocalidad.Clear();
+
+            this.LoadDepartamentos();
+        }
+
+        private async void btnEliminar_Click(object sender, EventArgs e)
+        {
+            string departamentoString = this.lstDepartamentos.SelectedItem.ToString();
+            string [] partes = departamentoString.Split(" - ");
+            int idDepartamento = int.Parse(partes[0].Trim());
+
+            int registros = await this.repo.DeleteDepartamentoAsync(idDepartamento);
+
+            MessageBox.Show("Registros eliminados: " + registros);
+
+            this.LoadDepartamentos();
+        }
+    }
+}
---
+++ NetCoreAdoNet/Repositories/RepositoryDepartamentos.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.Data.SqlClient;
+using NetCoreAdoNet.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace NetCoreAdoNet.Repositories
+{
+    public class RepositoryDepartamentos
+    {
+        private SqlConnection cn;
+        private SqlCommand com;
+        private SqlDataReader reader;
+
+        public RepositoryDepartamentos()
+        {
+            string connectionString = "Data Source=LOCALHOST\\DEVELOPER;Initial Catalog=HOSPITAL;Persist Security Info=True;User ID=SA;Trust Server Certificate=True";
+            this.cn = new SqlConnection(connectionString);
+            this.com = new SqlCommand();
+            this.com.Connection = cn;
+        }
+
+        public async Task<List<Departamento>> GetDepartamentosAsync()
+        {
+            string sql = "select * from DEPT";
+
+            this.com.CommandType = CommandType.Text;
+            this.com.CommandText = sql;
+            
+            await this.cn.OpenAsync();
+            
+            this.reader = await this.com.ExecuteReaderAsync();
+
+            List<Departamento> departamentos = new List<Departamento>();
+
+            while (await this.reader.ReadAsync())
+            {
+                Departamento departamento = new Departamento();
+                departamento.IdDepartamento = int.Parse(this.reader["DEPT_NO"].ToString());
+                departamento.Nombre = this.reader["DNOMBRE"].ToString();
+                departamento.Localidad = this.reader["LOC"].ToString();
+
+                departamentos.Add(departamento);
+            }
+
+            await this.reader.CloseAsync();
+            await this.cn.CloseAsync();
+
+            return departamentos;
+        }
+
+        public async Task<int> AddDepartamentoAsync(Departamento departamento)
+        {
+            string sql = "insert into DEPT (DEPT_NO, DNOMBRE, LOC) VALUES (@idDepartamento, @nombre, @localidad)";
+            SqlParameter idDepartamento = new SqlParameter("@idDepartamento", departamento.IdDepartamento);
+            SqlParameter nombre = new SqlParameter("@nombre", departamento.Nombre);
+            SqlParameter localidad = new SqlParameter("@localidad", departamento.Localidad);
+
+            this.com.CommandType = CommandType.Text;
+            this.com.CommandText = sql;
+
+            this.com.Parameters.Add(idDepartamento);
+            this.com.Parameters.Add(nombre);
+            this.com.Parameters.Add(localidad);
+
+            await this.cn.OpenAsync();
+
+            int registros = await this.com.ExecuteNonQueryAsync();
+
+            await this.cn.CloseAsync();
+            this.com.Parameters.Clear();
+
+            return registros;
+        }
+
+        public async Task<int> UpdateDepartamentoAsync(Departamento departamento) 
+        {
+            string sql = "update DEPT set DNOMBRE = @nombre, LOC = @localidad where DEPT_NO = @idDepartamento";
+            SqlParameter idDepartamento = new SqlParameter("@idDepartamento", departamento.IdDepartamento);
+            SqlParameter nombre = new SqlParameter("@nombre", departamento.Nombre);
+            SqlParameter localidad = new SqlParameter("@localidad", departamento.Localidad);
+
+            this.com.CommandType = CommandType.Text;
+            this.com.CommandText = sql;
+            this.com.Parameters.Add(idDepartamento);
+            this.com.Parameters.Add(nombre);
+            this.com.Parameters.Add(localidad);
+
+            await this.cn.OpenAsync();
+
+            int registros = await this.com.ExecuteNonQueryAsync();
+
+            await this.cn.CloseAsync();
+            this.com.Parameters.Clear();
+
+            return registros;
+        }
+
+        public async Task<int> DeleteDepartamentoAsync(int idDepartamento)
+        {
+            string sql = "delete from DEPT where DEPT_NO = @idDepartamento";
+            SqlParameter id = new SqlParameter("@idDepartamento", idDepartamento);
+
+            this.com.CommandType = CommandType.Text;
+            this.com.CommandText = sql;
+            this.com.Parameters.Add(id);
+
+            await this.cn.OpenAsync();
+            
+            int registros = await this.com.ExecuteNonQueryAsync();
+            
+            await this.cn.CloseAsync();
+            this.com.Parameters.Clear();
+
+            return registros;
+        }
+    }
+}
